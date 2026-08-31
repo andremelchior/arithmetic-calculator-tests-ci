9a4cdddf9b0c017100859dee2069e9c812d75349
André Melchior <68424109+andremelchior@users.noreply.github.com>
2026-08-31 08:43:11 -0300

    test: validate secret scanning

--- ArithmeticCalculator/ExternalService.cs
+++ ArithmeticCalculator/ExternalService.cs
@@ -5,5 +5,5 @@ namespace ArithmeticCalculator;
 public class ExternalService
 {
     //chave ficticia para simular exposicao
-    private const string ApiKey = "";
+    private const string ApiKey = "7f3a9c2e8b1d4f6a0c9e7b2d5a8f1c3e";
 }
