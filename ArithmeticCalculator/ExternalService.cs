4dc0dadb8456f1bbefd780f25220818f1c269328
André Melchior <68424109+andremelchior@users.noreply.github.com>
2026-08-31 08:59:17 -0300

    test: refactor GetUser to use SQL connection to validate CodeQL

--- ArithmeticCalculator/ExternalService.cs
+++ ArithmeticCalculator/ExternalService.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Linq;
+using System.Data.SqlClient;
 
 namespace ArithmeticCalculator;
 
 public class ExternalService
 {
-    //chave ficticia para simular exposicao
-    private const string ApiKey = "7f3a9c2e8b1d4f6a0c9e7b2d5a8f1c3e";
-
-    public async Task<User?> GetUser(string name)
+    public void GetUser(string name)
     {
-        var query = $"SELECT * FROM Users WHERE Name = '{name}'";
-    
-        return await Task.CompletedTask;
+        using var connection = new SqlConnection();
+        using var command = new SqlCommand(
+            $"SELECT * FROM Users WHERE Name = '{name}'",
+            connection
+        );
+
+        Console.WriteLine(command.CommandText);
     }
 }
