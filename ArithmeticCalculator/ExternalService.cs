6a947a7d713af2972e4e143a4bde9c52083ad402
André Melchior <68424109+andremelchior@users.noreply.github.com>
2026-08-31 09:15:48 -0300

    test: refactor GetUser method for SQL connection and query
    Updated SQL connection initialization and query construction to validate SQLi with CodeQL

--- ArithmeticCalculator/ExternalService.cs
+++ ArithmeticCalculator/ExternalService.cs
@@ -8,11 +8,13 @@ public class ExternalService
 {
     public void GetUser(string name)
     {
-        using var connection = new SqlConnection();
-        using var command = new SqlCommand(
-            $"SELECT * FROM Users WHERE Name = '{name}'",
-            connection
+        using var connection = new SqlConnection(
+            "Server=localhost;Database=Test;Integrated Security=True;"
         );
+
+        var query = "SELECT * FROM Users WHERE Name = '" + name + "'";
+
+        using var command = new SqlCommand(query, connection);
 
         Console.WriteLine(command.CommandText);
     }
