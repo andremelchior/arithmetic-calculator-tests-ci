f03f9f1e7a67ce67cb32922848153e142f68fd15
André Melchior <68424109+andremelchior@users.noreply.github.com>
2026-08-31 08:49:35 -0300

    test: validate CodeQL SQL injection detection

--- ArithmeticCalculator/ExternalService.cs
+++ ArithmeticCalculator/ExternalService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace ArithmeticCalculator;
 
@@ -6,4 +7,11 @@ public class ExternalService
 {
     //chave ficticia para simular exposicao
     private const string ApiKey = "7f3a9c2e8b1d4f6a0c9e7b2d5a8f1c3e";
+
+    public async Task<User?> GetUser(string name)
+    {
+        var query = $"SELECT * FROM Users WHERE Name = '{name}'";
+    
+        return await Task.CompletedTask;
+    }
 }
